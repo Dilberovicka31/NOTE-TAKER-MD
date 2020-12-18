687aa3515d65040e28e6d6d5f119c76971bb2290
Dilberovicka31 <dilberovicm36@gmail.com>
2020-12-17 18:48:30 -0800

    created api routes to get and post

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
+const { nanoid } = require("nanoid");
 const app = express();
+
 const PORT = process.env.PORT || 8080;
 app.use(express.static("public"));
 
@@ -10,35 +12,52 @@ app.use(express.json());
 
 //Routes
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, ""));
+app.get("/api/notes", function (req, res) {
+  const readFile = JSON.parse(
+    fs.readFileSync("db/db.json", {
+      encoding: "utf-8",
+    })
+  );
+  console.log(readFile);
+  res.json(readFile);
+});
+
+app.post("/api/notes", function (req, res) {
+  console.log(req.body);
+
+  const readFile = JSON.parse(
+    fs.readFileSync("db/db.json", {
+      encoding: "utf-8",
+    })
+  );
+  const noteSave = {
+    title: req.body.title,
+    text: req.body.text,
+    id: nanoid(),
+  };
+  readFile.push(noteSave);
+
+  fs.writeFileSync("db/db.json", JSON.stringify(readFile));
+  res.json(readFile);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  console.log(req.params.id);
+  //Access the :id from req.params.id
+  //Use fs module to read the file
+  //Then parse teh file contents  JSON.parse
+  //Use the array.filter to filter out th id
+  // myArray = myArray.filter(({ id }) => id !== req.params.id);
 });
 
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", function (req, res) {});
-app.post("/api/notes", function (req, res) {
-  //Access the POSTed data in req.body
-  //Use the fs module to read the file
-  //Parse the file contents with JSON.parse()
-  //Push req.body to the array list
-  //JSN.stringify the array list back into a JSON string
-  //Then save the contents back to teh db.json wtih fs
-});
-
-app.delete("/api/notes/:id", function (req, res) {
-  //Access the :id from req.params.id
-  //Use fs module to read the file
-  //Then parse teh file contents  JSON.parse
-
-  //Use the array.filter to filter out teh id
-  myArray = myArray.filter(({ id }) => id !== req.params.id);
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, function () {
   console.log("App listening on PORT:" + PORT);
 });
-
-//STORE object for doing all the
